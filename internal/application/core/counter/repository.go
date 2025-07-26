4a47ac91c04d5b99dbf4d7432a5295bbee04d030
Omar Eloui <contact@omareloui.com>
2025-07-26 15:25:11 +0300

    fix(counter): fix the counter repository interface

--- internal/application/core/counter/repository.go
+++ internal/application/core/counter/repository.go
@@ -1,8 +1,6 @@
 package counter
 
 type CounterRepository interface {
-	CreateCounter(*Counter) error
-	GetCounter() (*Counter, error)
 	AddOneToProduct(category string) (uint8, error)
 	AddOneToOrder() (uint, error)
 }
